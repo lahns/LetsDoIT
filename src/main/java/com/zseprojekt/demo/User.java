ebb48b4de5cf621f261456db3e1ff93665f47d10
lahns <megaice@interia.pl>
2022-10-11 22:24:09 +0200

    init

--- src/main/java/com/zseprojekt/demo/User.java
+++ src/main/java/com/zseprojekt/demo/User.java
@@ -5,12 +5,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class User {
+    @Id
+    @GeneratedValue
     private int id;
     private String username;
     private String password; //TODO - hashowac haslo(albo jakkolwiek je szyfrowac)
